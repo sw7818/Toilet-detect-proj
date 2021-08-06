ff8cee45c700a637dac910ce80d506669205c515
sw7818 <77532769+sw7818@users.noreply.github.com>
2021-08-06 13:44:22 +0100

    Add files via upload

--- laser_test.ino
+++ laser_test.ino
@@ -1,10 +1,10 @@
+
+// Defined variables 
+
 int pinReceiver1 = 6; 
 int pinReceiver2 = 5; 
 int x = 0;
-int capacity = 5;
-
 int cap = 3;
-char buffer[100] = {0};
 
 
 #include "Nextion.h"
@@ -13,14 +13,15 @@ char buffer[100] = {0};
 
 
 NexText tEnter = NexText(0, 5, "tEnter"); 
-NexText tOcc = NexText(0, 8, "tOcc"); 
-NexText tEwait = NexText(0, 9, "tEwait");
-NexText tCap = NexText(0, 10, "tCap");
-NexText tCap2 = NexText(1, 4, "tCap2");
 NexButton bcapacity = NexButton(0, 3, "bcapacity");
 NexButton b2 = NexButton(1, 7, "b2");
-NexButton bplus = NexButton(1, 5, "bplus");
-NexButton bminus = NexButton(1, 6, "bminus");
+NexButton bplus = NexButton(1, 4, "bplus");
+NexButton bminus = NexButton(1, 5, "bminus");
+
+// these are the pages 
+
+NexPage page0 = NexPage(0, 0, "page0");  // Page added as a touch event
+NexPage page1 = NexPage(1, 0, "page1");  // Page added as a touch event
 
 
 NexTouch *nex_listen_list[] = {
@@ -32,34 +33,63 @@ NexTouch *nex_listen_list[] = {
 };
 
 
+void bplusPushCallback(void *ptr) // press event for plush button
+{
+  
+  cap = cap +1;
+  Serial.print("n2.val=");  // This is sent to the nextion display to set what object name (before the dot) and what atribute (after the dot) are you going to change.
+  Serial.print(cap);  // This is the value you want to send to that object and atribute mentioned before.
+  Serial.write(0xff);  // We always have to send this three lines after each command sent to the nextion display.
+  Serial.write(0xff);
+  Serial.write(0xff);
+  
+  }
+
+
+void bminusPushCallback(void *ptr) // press event for plush button
+{
+  
+  cap = cap -1;
+  Serial.print("n2.val=");  
+  Serial.print(cap);  
+  Serial.write(0xff);  
+  Serial.write(0xff);
+  Serial.write(0xff);
+  
+  }
 
 
 
-// so we need to keep on :
-//    1. updating the occupancy of the toilet
-//    2. update the wait time 
-//    3. Room capacity page0 
-//    4. Room capacity page1 
+
+// update the wait time ___________________________________________________________________________
+
 void setup() {
   Serial.begin(9600);
 
+  Serial.print("baud=115200");  // Set new baud rate of nextion to 115200, but it's temporal. Next time nextion is power on,
+                                // it will retore to default baud of 9600.
+                                // To take effect, make sure to reboot the arduino (reseting arduino is not enough).
+                                // If you want to change the default baud, send the command as "bauds=115200", instead of "baud=115200".
+                                // If you change the default baud, everytime the nextion is power ON is going to have that baud rate, and
+                                // would not be necessery to set the baud on the setup anymore.
+  Serial.write(0xff);  // We always have to send this three lines after each command sent to nextion.
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.begin(115200);  // Start serial comunication at baud=115200
+
+
   pinMode(pinReceiver1, INPUT);
   pinMode(pinReceiver2, INPUT);
-
   nexInit();
 
-  bplus.attachPop(btnPlusPopCallback, &bplus); 
-  bminus.attachPop(btnMinusPopCallback, &bminus);
-
-  
-  
+  bplus.attachPush(bplusPushCallback);
+  bminus.attachPush(bplusPushCallback);
 
 }
 
-
-
 void loop() {
 
+  delay(30);
   int value_1 = digitalRead(pinReceiver1); 
   int value_2 = digitalRead(pinReceiver2);
   
@@ -69,7 +99,15 @@ void loop() {
     if (value_2 ==1){
     Serial.println("Oi someones coming in");
     x++;
-    tOcc.setText(x);
+    //tOcc.setText(x);
+    Serial.print("n0.val="); 
+    Serial.print(x) ;
+    Serial.write(0xff);  
+    Serial.write(0xff);
+    Serial.write(0xff);
+
+    
+    
     while((value_1 == 0) || (value_2 ==0)){
     value_1 = digitalRead(pinReceiver1); 
     value_2 = digitalRead(pinReceiver2);
@@ -83,25 +121,29 @@ void loop() {
     if (value_1 ==1){
     Serial.println("Oi someones coming out");
     x--;
-    tOcc.setText(x);
+    //tOcc.setText(x);
+
+    Serial.print("n0.val=");
+    Serial.print(x);   
+    Serial.write(0xff);  
+    Serial.write(0xff);
+    Serial.write(0xff);
     while((value_1 == 0) || (value_2 ==0)){
       value_1 = digitalRead(pinReceiver1); 
       value_2 = digitalRead(pinReceiver2);
       
     }
     }
-  
+  nexLoop(nex_listen_list); // checks for any touch action on the screen 
 
-Serial.println("The number of people in the toilet is:   ");
-Serial.print(x);
 
-if (x==5){
-  tEnter.setText("Unavailable");
+
 }
+  
+
 
 
 
   
   
-}
 }
